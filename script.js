df6ecfc94a1c20047abbbe7a16ef38ea06ddbc21
Giovanna Marques <giovannamrodrigues2006@gmail.com>
2026-04-19 20:51:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-const particles = document.getElementById("particles")
+const particles = document.getElementById("particles");
 
 function createParticle(){
+    const particle = document.createElement("div");
 
-const particle=document.createElement("div")
+    particle.style.position = "absolute";
+    particle.style.width = "3px";
+    particle.style.height = "3px";
+    particle.style.background = "#577ae4";
+    particle.style.left = Math.random() * window.innerWidth + "px";
+    particle.style.top = "0";
+    particle.style.transition = "transform 6s linear";
 
-particle.style.position="absolute"
-particle.style.width="3px"
-particle.style.height="3px"
-particle.style.background="#577ae4"
+    particles.appendChild(particle);
 
-particle.style.left=Math.random()*window.innerWidth+"px"
-particle.style.top="0"
+    setTimeout(() => {
+        particle.style.transform = "translateY(100vh)";
+    }, 10);
 
-particle.style.transition="transform 6s linear"
-
-particles.appendChild(particle)
-
-setTimeout(()=>{
-
-particle.style.transform="translateY(100vh)"
-
-},10)
-
-setTimeout(()=>{
-
-particle.remove()
-
-},6000)
-
+    setTimeout(() => {
+        particle.remove();
+    }, 6000);
 }
 
-setInterval(createParticle,200)
-
+setInterval(createParticle, 200);
 
 function attack(){
+    const knight = document.querySelector(".knight");
 
-const knight=document.querySelector(".knight")
+    knight.style.transform = "scale(1.15) rotate(-8deg)";
 
-knight.style.transform="scale(1.2) rotate(-10deg)"
-
-setTimeout(()=>{
-
-knight.style.transform="scale(1)"
-
-},200)
-
+    setTimeout(() => {
+        knight.style.transform = "";
+    }, 200);
 }
